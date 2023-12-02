463cbfcb5c77d28dee8b0b80d39482f6350f3fd3
Conscience Osakue <consciencenosa@gmail.com>
2023-12-01 22:26:38 -0600

    initial commit

---
+++ ConsoleUI/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI
+{
+    public class Car : Vehicle
+    {
+        public bool HasTrunk { get; set; }
+        public override void DriveAbstract()
+        {
+            Console.WriteLine("The car is driving ..");
+
+        }
+
+    }
+}
